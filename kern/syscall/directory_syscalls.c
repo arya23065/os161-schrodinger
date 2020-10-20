0d20d69731a5bcd6e82c16e37e97a2d2244b7a16
arya23065 <arya.subramanyam@alumni.ubc.ca>
2020-10-20 00:10:04 -0700

    Added chdir

--- kern/syscall/directory_syscalls.c
+++ kern/syscall/directory_syscalls.c
@@ -1,14 +1,28 @@
 #include <types.h>
-// #include <unistd.h>
-// #include <syscall.h>
+#include <lib.h>
+#include <copyinout.h>
+#include <syscall.h>
+#include <current.h>
+#include <limits.h>
+#include <vnode.h>
+#include <vfs.h>
 
+int
+sys_chdir(const_userptr_t pathname, int *retval) {
+    
+    char pname[PATH_MAX];
 
-// int
-// sys_chdir(const char *pathname)
-// {
+	copyinstr(pathname, pname, PATH_MAX, NULL);
+
+    int err = vfs_chdir(pname); 
+
+    if (err != 0) {
+        *retval = err; 
+        return -1; 
+    }
 	
-// 	return 0;
-// }
+	return 0;
+}
 
 
 // int
